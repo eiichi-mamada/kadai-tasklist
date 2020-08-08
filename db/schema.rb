2804abc94a944985308e2c7d79f9a8396fa4061b
eiichi-mamada <eastrage@gmail.com>
2020-08-08 09:36:08 +0000

    statusカラムの追加とタスクの状態を表示

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_07_051842) do
+ActiveRecord::Schema.define(version: 2020_08_08_050615) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
