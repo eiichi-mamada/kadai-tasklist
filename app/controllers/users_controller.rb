1f61ce673260e662874d98292f43beaa34a03c65
eiichi-mamada <eastrage@gmail.com>
2020-08-12 07:45:06 +0000

    ユーザー登録機能の追加

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,57 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+    
+  def index
+    @users = User.order(id: :desc).page(params[:page]).per(10)
+  end
+  
+  def show
+  end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = 'ユーザを登録しました。'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'ユーザの登録に失敗しました。'
+      render :new
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @user.save
+      flash[:success] = 'ユーザを更新しました。'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'ユーザの更新に失敗しました。'
+      render :new
+    end
+  end
+  
+  def destroy
+    @user.destroy
+
+    flash[:success] = 'ユーザーは正常に削除されました'
+    redirect_to users_url
+  end
+  
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  
+end
