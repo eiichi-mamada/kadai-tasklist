63f9fcc01b27d4c4e6e3c1d1418438b9c2368247
eiichi-mamada <eastrage@gmail.com>
2020-08-13 02:53:56 +0000

    login機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   root to: 'tasks#index'
-
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   resources :tasks
   
+
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
 end
